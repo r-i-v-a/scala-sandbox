2732da8ffde35317c238690762f14796d683b966
Riva Nathans <rnathans-cf@dnanexus.com>
2021-03-27 18:38:00 +0100

    Reversort cost, test case works.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,8 +8,36 @@ object Main extends App {
 	System.setIn(new FileInputStream("src/resources/in.txt"))
 	System.setOut(new PrintStream(new FileOutputStream("src/resources/out.txt")))
 
+	def reverseInterval(a: Array[Int], i: Int, j: Int): Array[Int] = {
+		if (i < j) {
+			var tmp = a(i)
+			a(i) = a(j)
+			a(j) = tmp
+			reverseInterval(a, i+1, j-1)
+		} else {
+			a
+		}
+	}
+
 	def calculateCost(a: Array[Int]): Int = {
-		a(0)
+		var b = a
+		var cost = 0
+
+		for (i <- 0 until b.length - 1) {
+			var min = b(i)
+			var minIdx = i
+			for (j <- i until b.length) {
+				if (b(j) < min) {
+					min = b(j)
+					minIdx = j
+				}
+			}
+
+			cost += minIdx - i + 1
+			b = reverseInterval(b, i, minIdx)
+		}
+
+		cost
 	}
 
 	val cases = readLine().toInt
